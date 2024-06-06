146efe793f0e34cb3c33a4afde851af53471fdc9
anhtuankckt <anhtuankckt@gmail.com>
2024-06-06 19:44:41 +0700

    fix: server: package.json

--- server/src/server.js
+++ server/src/server.js
@@ -23,7 +23,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
-app.use(express.json({ limit: '5mb' }))
+app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cookieParser())
